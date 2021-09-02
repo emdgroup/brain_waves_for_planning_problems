9be637d2ec4cdcc685da54de6fb6cefc5cb180a2
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-09-02 09:29:36 +0200

    fix style and typos

--- Hybrid_Neuron_Simulation.py
+++ Hybrid_Neuron_Simulation.py
@@ -1,7 +1,7 @@
 """
 Attractor Network for 2DoF Robot Arm
 
-Author: Henry Powell and Mathias Winkle
+Author: Henry Powell and Mathias Winkel
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,9 +21,9 @@ tau = 0.8
 
 def update_place_cell_synapses(x: np.ndarray = None,
                                place_cell_synapses: np.ndarray = None):
-    
+
     for i in range(place_cell_synapses.shape[0]):
-        
+
         iidx = np.unravel_index(i, (place_cell_x, place_cell_y))
         ix = iidx[0]
         iy = iidx[1]
@@ -38,38 +38,38 @@ def update_place_cell_synapses(x: np.ndarray = None,
         cj_x = (jx - 0.5) / place_cell_x
         cj_y = (jy - 0.5) / place_cell_x
         cj = np.array([cj_x, cj_y]).T
-        
+
         diff = ci - cj
         diff += x
-        
+
         norm = np.linalg.norm(diff, axis=1)
-        
+
         place_cell_synapses[i] = J * np.exp(-(norm**2/sigma**2)) - T
-            
-    
+
     return place_cell_synapses.T
 
 
 def update_place_cell_activations(place_cell_synapses: np.ndarray = None,
                                   place_cell_activations: np.ndarray = None):
-    
+
     place_cell_activations = place_cell_activations.flatten()
     B = np.dot(place_cell_activations, place_cell_synapses)
-    
+
     summed_activations = np.sum(place_cell_activations)
-    
+
     if summed_activations > 0:
         place_cell_activations = (1 - tau) * B + (tau * (B/summed_activations))
-    
+
     else:
         place_cell_activations = np.zeros_like(B)
-    
+
     place_cell_activations[place_cell_activations < 0] = 0
-    
+
     return place_cell_activations.reshape(place_cell_x, place_cell_y)
 
 
-def imprint_circular_kernel(field: np.array, layer_from: int, layer_to: int, radius: int, max_value: float, center_value: float, center_radius: int = 0, power: int = 1) -> np.array:
+def imprint_circular_kernel(field: np.array, layer_from: int, layer_to: int, radius: int, max_value: float,
+                            center_value: float, center_radius: int = 0, power: int = 1) -> np.array:
 
     assert field.shape[1:3] == field.shape[-2:]
 
@@ -107,17 +107,17 @@ re = np.random.uniform(0, 1, (ne, 1))
 # ri = np.zeros((ni, 1)) # fix this (1/0)
 ri = np.random.uniform(0, 1, (ne, 1))
 
-a = np.append(np.array([0.02 * np.ones((ne, 1))]),               # time scale of the recovery variable u
+a = np.append(np.array([0.02 * np.ones((ne, 1))]),                # time scale of the recovery variable u
               np.array([0.02 + 0.08 * ri]))[:, np.newaxis]
 
-b = np.append(np.array([0.2 * np.ones((ne, 1))]),                # sensitivity of the recovery variable u to the subthreshold
-              np.array([0.25 - 0.05 * ri]))[:, np.newaxis]       # fluctuations of the membrane potential v.
+b = np.append(np.array([0.2 * np.ones((ne, 1))]),                 # sensitivity of the recovery variable u to the subthreshold
+              np.array([0.25 - 0.05 * ri]))[:, np.newaxis]        # fluctuations of the membrane potential v.
 
-c = np.append(np.array([-65 + 15 * re**2]),                      # after-spike reset value of the membrane
-              np.array([-65 * np.ones((ni, 1))]))[:, np.newaxis] # potential v caused by the fast high-threshold K+ conductances.
+c = np.append(np.array([-65 + 15 * re**2]),                       # after-spike reset value of the membrane
+              np.array([-65 * np.ones((ni, 1))]))[:, np.newaxis]  # potential v caused by the fast high-threshold K+ conductances.
 
-d = np.append(np.array([8 - 6 * re**2]),                         # after-spike reset of the recovery variable
-              np.array([2*np.ones((ni, 1))]))[:, np.newaxis]     # u caused by slow high-threshold Na+ and K+ conductances.
+d = np.append(np.array([8 - 6 * re**2]),                          # after-spike reset of the recovery variable
+              np.array([2*np.ones((ni, 1))]))[:, np.newaxis]      # u caused by slow high-threshold Na+ and K+ conductances.
 
 a = a.reshape((2, size, size))
 b = b.reshape((2, size, size))
@@ -172,11 +172,11 @@ I[(0, *target_neuron)] = 10
 # Construct continuous attractor layer
 place_cell_synapses = np.zeros((place_cell_x * place_cell_y, place_cell_x * place_cell_y)) 
 
-direc = np.array([10,10]) / np.array([place_cell_x, place_cell_y])
+direc = np.array([10, 10]) / np.array([place_cell_x, place_cell_y])
 
 np.random.seed(2) 
 place_cell_activations = np.random.uniform(0, 1 / np.sqrt(place_cell_x * place_cell_y),
-                                          place_cell_x * place_cell_y).reshape(place_cell_x, place_cell_y)
+                                           place_cell_x * place_cell_y).reshape(place_cell_x, place_cell_y)
 
 trajectory = np.zeros((size, size))
 
@@ -189,14 +189,14 @@ max_plot = list()
 
 for t in range(1000):
     place_cell_synapses = update_place_cell_synapses(direc, place_cell_synapses)
-    
+
     place_cell_activations = update_place_cell_activations(place_cell_synapses,
-                                                            place_cell_activations)
+                                                           place_cell_activations)
     place_cell_activations = np.multiply(place_cell_activations, PC_inactive)
-    
+
     # normalize place cell activations to prevent 
     place_cell_activations /= np.max(place_cell_activations)
-    
+
     spiking_fired = v >= 30
     spiking_fired_excite = np.where(v[:ne] >= 30)[0]
 
@@ -205,7 +205,7 @@ for t in range(1000):
 
     overlap = np.multiply(place_cell_activations, fire_grid)
     overlap_coordinates = np.argwhere(overlap > 0)
-    
+
     if len(overlap_coordinates) == 0:
         direc = np.array([0, 0])
     else:
@@ -222,9 +222,9 @@ for t in range(1000):
         trajectory *= 0.95
         trajectory[tuple(np.round(average_spiking_pos).astype(int))] = 1.
         trajectory[place_cell_peak] = -1.
-    
+
     zs = np.zeros((2, size, size))
-    
+
     # reset SNN neurons that spiked and compute their output current towards the other neurons
     for _i in np.argwhere(spiking_fired):
         i = tuple(_i)
@@ -249,48 +249,48 @@ for t in range(1000):
     fig.clear()
     fig.suptitle(t)
 
-    plt.subplot(2,4,1)
+    plt.subplot(2, 4, 1)
     plt.title('Exci. Firing Pattern', fontsize=8)
     plt.imshow(fire_grid, vmin=0, vmax=2)
     plt.xticks([])
     plt.yticks([])
-    
-    plt.subplot(2,4,2)
+
+    plt.subplot(2, 4, 2)
     plt.title('Exci. SNN Membrane Potential', fontsize=8)
     plt.imshow(v[0], vmin=-70, vmax=30)
     plt.xticks([])
     plt.yticks([])
-    
-    plt.subplot(2,4,3)
+
+    plt.subplot(2, 4, 3)
     plt.title('Inhi. Firing Pattern', fontsize=8)
     plt.imshow(1 * spiking_fired[1], vmin=0, vmax=2)
     plt.xticks([])
     plt.yticks([])
-    
-    plt.subplot(2,4,4)
+
+    plt.subplot(2, 4, 4)
     plt.title('Inhi. SNN Membrane Potential', fontsize=8)
     plt.imshow(v[1], vmin=-70, vmax=30)
     plt.xticks([])
     plt.yticks([])
-    
-    plt.subplot(2,4,5)
+
+    plt.subplot(2, 4, 5)
     plt.title('Place Cell Activations', fontsize=8)
     plt.imshow(place_cell_activations)
     plt.xticks([])
     plt.yticks([])
-    
-    plt.subplot(2,4,6)
+
+    plt.subplot(2, 4, 6)
     plt.title('Overlap', fontsize=8)
     plt.imshow(overlap)
     plt.xticks([])
     plt.yticks([])
-    
-    plt.subplot(2,4,7)
+
+    plt.subplot(2, 4, 7)
     plt.title('Trajectory', fontsize=8)
     plt.imshow(trajectory, vmin=-1, vmax=1, cmap='bwr')
     plt.xticks([])
     plt.yticks([])
-        
+
     # plt.subplot(2,1,1)
     # if len(coords) > 0:
     #     y, x = zip(*coords)
@@ -301,24 +301,20 @@ for t in range(1000):
     # plt.ylim(0, size*size)
     # plt.xlabel("Time (ms)")
     # plt.ylabel("Neuron Index")
-    
+
     # max_plot.append(np.sum(fire_grid))
     # plt.subplot(2,1,2)
     # plt.plot(max_plot)
-    
+
     plt.show()
-    
+
     plt.tight_layout()
 
     plt.pause(0.1)
-    animation.add_frame(fig) # comment to prevent saving plots to disc
-    
+    animation.add_frame(fig)  # comment to prevent saving plots to disc
+
     if not plt.fignum_exists(fig.number):
         print('Figure closed. Finalizing simulation.')
         break
-    
+
 animation.save(__file__, fps=4, keep_frame_images=False)
-
-
-    
-
